607fb6077083aa67d6c93f2ad8fda33d2e9af212
MahbubMorshed <mahbub.bogra30@gmail.com>
2022-07-24 17:34:16 -0700

    error solving

--- shopbd/settings.py
+++ shopbd/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
